03b604126ecb356f390c42283e8567e62c463a01
MinhowS <lxdliudong@hotmail.com>
2017-12-13 02:12:54 +0800

    v1.0

--- manager/src/App.vue
+++ manager/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+
     <router-view/>
   </div>
 </template>
@@ -11,13 +11,40 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  body {
+    margin: 0;
+    padding: 0;
+    font-size: 14px;
+    -webkit-font-smoothing: antialiased;
+    overflow: scroll;
+    overflow-y: hidden;
+  }
+
+  #app {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+  }
+
+  .el-submenu [class^=fa] {
+    vertical-align: baseline;
+    margin-right: 10px;
+  }
+
+  .el-menu-item [class^=fa] {
+    vertical-align: baseline;
+    margin-right: 10px;
+  }
+
+  .toolbar {
+    background: #f2f2f2;
+    padding: 10px;
+    margin: 10px 0;
+    .el-form-item {
+      margin-bottom: 10px;
+    }
+  }
+
 </style>
--- manager/src/router/index.js
+++ manager/src/router/index.js
@@ -1,15 +1,55 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import login from '@/components/login'
+import home from '@/components/home'
+import notFound from '@/components/notFound'
+import menuTab from '@/components/menu1/menuTab'
+import menuTable from '@/components/menu2/menuTable'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+   /* {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },*/
+      {
+          path: '/login',
+          name: 'login',
+          component: login,
+          hidden:true
+      },
+      {
+          path: '/notFound',
+          name: 'notFound',
+          component: notFound,
+          hidden:true
+      },
+      {
+          path: '/',
+          name: '导航一',
+          component: home,
+          iconCls:'el-icon-message',//图标样式class
+          children:[{
+            path:'/menutab',
+            component:menuTab,
+            name:'Tab'
+          }]
+      },
+      {
+          path: '/',
+          name: '导航二',
+          component: home,
+          iconCls:'fa fa-id-card-o',//图标样式class
+          children:[{
+              path:'/menutable',
+              component:menuTable,
+              name:'Table'
+          }]
+      }
+
   ]
 })
